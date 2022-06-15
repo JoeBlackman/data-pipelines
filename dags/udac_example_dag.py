917a395da74e79266abafae036fa33913606ca8d
unknown <jblackman.prof@gmail.com>
2022-06-15 11:17:51 -0400

    satisfied DAG configuration of rubric

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -11,16 +11,25 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'udacity',
+    'Depends_on_past': False,
     'start_date': datetime(2019, 1, 12),
+    'Retries': 1,
+    'Retry_delay': timedelta(minutes=5),
+    'Catchup': False
 }
 
-dag = DAG('udac_example_dag',
-          default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
+dag = DAG(
+    'udac_example_dag',
+    default_args=default_args,
+    description='Load and transform data in Redshift with Airflow',
+    schedule_interval=timedelta(hours=1),
+    tags=['udacity', 'data-pipelines'],
+)
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+start_operator = DummyOperator(
+    task_id='Begin_execution', 
+    dag=dag
+)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
@@ -62,7 +71,10 @@ run_quality_checks = DataQualityOperator(
     dag=dag
 )
 
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+end_operator = DummyOperator(
+    task_id='Stop_execution', 
+    dag=dag
+)
 
 # Task dependencies defined here
 start_operator >> stage_events_to_redshift
